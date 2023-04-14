7193e7ccb01ef74a5d78995fb3c43c39ca5a1b27
kaifkalanthar <77534625+kaifkalanthar@users.noreply.github.com>
2023-04-14 22:34:53 +0530

    landing-page added

--- curly_b/src/App.js
+++ curly_b/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from './components/navBar';
 import HeroSection from './components/heroSection';
+import RecentBlogs from './components/recentBlogs';
 
 function App() {
   return (
     <React.Fragment>
       <NavBar />
       <HeroSection />
+      <RecentBlogs />
     </React.Fragment >
   );
 }
--- curly_b/src/index.js
+++ curly_b/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.css";
-import './style.scss';
+import './style.css';
 import App from './App';
 
 
