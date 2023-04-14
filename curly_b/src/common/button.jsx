440d1847fdd3f67d76d3dc5dc42cd40615fa19a8
kaifkalanthar <77534625+kaifkalanthar@users.noreply.github.com>
2023-04-14 14:04:15 +0530

    v1 deleted

--- curly_b/src/common/button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Button = ({ label, disabled }) => {
-    return (
-        <button type="submit" id="submit-btn" disabled={disabled}>{label}</button>
-    );
-}
-
-export default Button;
