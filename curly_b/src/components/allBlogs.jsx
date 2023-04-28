c0bced13d4445a80887d4b329ecbfa28d506a552
Mohamed_Kaif17 <77534625+kaifkalanthar@users.noreply.github.com>
2023-04-29 01:22:22 +0530

    Update allBlogs.jsx

--- curly_b/src/components/allBlogs.jsx
+++ curly_b/src/components/allBlogs.jsx
@@ -9,7 +9,7 @@ const AllBlogs = () => {
     const getData = async () => {
         const { apiUrl: ApiUrl } = apiUrl;
         const apiEndpoint = ApiUrl + "api/post";
-        const { data: res } = await http.get("http://localhost:4000/post");
+        const { data: res } = await http.get("https://curly-b-xgc0.onrender.com/post");
         setData(res);
     }
     useEffect(() => {
@@ -19,7 +19,7 @@ const AllBlogs = () => {
         <div className="blogs-container">
             {data.map(d => (
                 <div className="blog-card" key={d._id}>
-                    <img src={"http://localhost:4000/" + d.cover} alt="" className="card-img" />
+                    <img src={"https://curly-b-xgc0.onrender.com/" + d.cover} alt="" className="card-img" />
                     <div className="card-content">
                         <h2 className="blog-title">{d.title}</h2>
                         <p className="timestamp">MohamedKaif. 31 Jan</p>
