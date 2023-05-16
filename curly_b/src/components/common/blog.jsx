81cf2b20bfb48bd6c1929f759e935816f601ac62
Mohamed_Kaif17 <77534625+kaifkalanthar@users.noreply.github.com>
2023-05-17 01:41:34 +0530

    Update blog.jsx

--- curly_b/src/components/common/blog.jsx
+++ curly_b/src/components/common/blog.jsx
@@ -4,7 +4,7 @@ const Blog = ({ title, content, cover }) => {
     return (
         <div className="post-container" >
             <h1 className="post-header" >{title}</h1>
-            <img src={"https://curly-b-xgc0.onrender.com/" + cover} className="post-img" alt="" />
+            <img src={"https://curly-b-api.onrender.com/" + cover} className="post-img" alt="" />
             <div dangerouslySetInnerHTML={{ __html: content }} className="post-content" />
         </div>
     );
