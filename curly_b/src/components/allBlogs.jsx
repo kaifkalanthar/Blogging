e49a151dd9dc7e0e084f90342b3d8d25cca8116a
kaifkalanthar <77534625+kaifkalanthar@users.noreply.github.com>
2023-05-17 01:09:04 +0530

    apiurl changed

--- curly_b/src/components/allBlogs.jsx
+++ curly_b/src/components/allBlogs.jsx
@@ -5,21 +5,24 @@ import http from '../service/httpService';
 import apiUrl from '../config.json';
 const AllBlogs = () => {
     const [data, setData] = useState([]);
+    const { apiUrl: ApiUrl } = apiUrl;
+    const apiEndpoint = ApiUrl + "post";
 
     const getData = async () => {
-        const { apiUrl: ApiUrl } = apiUrl;
-        const apiEndpoint = ApiUrl + "api/post";
-        const { data: res } = await http.get("http://localhost:4000/post");
+
+        const { data: res } = await http.get(apiEndpoint);
         setData(res);
     }
     useEffect(() => {
         getData();
-    })
+    }, [])
+
+
     return ( 
         <div className="blogs-container">
             {data.map(d => (
                 <div className="blog-card" key={d._id}>
-                    <img src={"http://localhost:4000/" + d.cover} alt="" className="card-img" />
+                    <img src={ApiUrl + d.cover} alt="" className="card-img" />
                     <div className="card-content">
                         <h2 className="blog-title">{d.title}</h2>
                         <p className="timestamp">MohamedKaif. 31 Jan</p>
--- curly_b/src/components/common/blog.jsx
+++ curly_b/src/components/common/blog.jsx
@@ -4,7 +4,7 @@ const Blog = ({ title, content, cover }) => {
     return (
         <div className="post-container" >
             <h1 className="post-header" >{title}</h1>
-            <img src={"http://localhost:4000/" + cover} className="post-img" alt="" />
+            <img src={"https://curly-b-api.onrender.com/" + cover} className="post-img" alt="" />
             <div dangerouslySetInnerHTML={{ __html: content }} className="post-content" />
         </div>
     );
