6baf55927f1af1a8f297c59cc1faee1ef7d0ff5a
kaifkalanthar <77534625+kaifkalanthar@users.noreply.github.com>
2023-05-25 01:47:09 +0530

    pack.json

--- curly_b/src/components/allBlogs.jsx
+++ curly_b/src/components/allBlogs.jsx
@@ -24,7 +24,6 @@ const AllBlogs = () => {
         <div className="blogs-container">
             {data.map(d => (
                 <div className="blog-card" key={d._id}>
-
                     <img src={ApiUrl + d.cover} alt="" className="card-img" />
                     <div className="card-content">
                         <h2 className="blog-title">{d.title}</h2>
@@ -34,47 +33,6 @@ const AllBlogs = () => {
                     </div>
                 </div>
             ))}
-            {/* <div className="blog-card">
-                <img src={saul} alt="" className="card-img" />
-                <div className="card-content">
-                    <h2 className="blog-title">All About Walter</h2>
-                    <p className="timestamp">MohamedKaif. 31 Jan</p>
-                    <p className="blog-description">an American crime drama television series created and produced by Vince Gilligan.
-                    Set and filmed in Albuquerque, New Mexico, the series follows Walter White</p>
-                    <button className="btn">More...</button>
-                </div>
-            </div>
-            <div className="blog-card">
-                <img src={saul} alt="" className="card-img" />
-                <div className="card-content">
-                    <h2 className="blog-title">All About Walter</h2>
-                    <p className="timestamp">MohamedKaif. 31 Jan</p>
-                    <p className="blog-description">an American crime drama television series created and produced by Vince Gilligan.
-                    Set and filmed in Albuquerque, New Mexico, the series follows Walter White</p>
-                    <button className="btn">More...</button>
-                </div>
-            </div>
-            <div className="blog-card">
-                <img src={saul} alt="" className="card-img" />
-                <div className="card-content">
-                    <h2 className="blog-title">All About Walter</h2>
-                    <p className="timestamp">MohamedKaif. 31 Jan</p>
-                    <p className="blog-description">an American crime drama television series created and produced by Vince Gilligan.
-                    Set and filmed in Albuquerque, New Mexico, the series follows Walter White</p>
-                    <button className="btn">More...</button>
-                </div>
-            </div>
-            <div className="blog-card">
-                <img src={saul} alt="" className="card-img" />
-                <div className="card-content">
-                    <h2 className="blog-title">All About Walter</h2>
-                    <p className="timestamp">MohamedKaif. 31 Jan</p>
-                    <p className="blog-description">an American crime drama television series created and produced by Vince Gilligan.
-                    Set and filmed in Albuquerque, New Mexico, the series follows Walter White</p>
-                    <button className="btn">More...</button>
-                </div>
-            </div> */}
-            
         </div>
      );
 }
