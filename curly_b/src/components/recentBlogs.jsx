d8001f4f146248fd6781a25f8a6f50140053e59f
kaifkalanthar <77534625+kaifkalanthar@users.noreply.github.com>
2023-05-27 04:49:20 +0530

    img-added

--- curly_b/src/components/recentBlogs.jsx
+++ curly_b/src/components/recentBlogs.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import blogpost1 from '../assets/blog-post1.svg';
-import blogpost2 from '../assets/blog-post2.svg';
-import blogpost3 from '../assets/blog-post3.svg';
+import blogpost2 from '../assets/blog-post2.jpg';
+import blogpost3 from '../assets/blog-post3.jpg';
 import http from '../service/httpService';
 import apiUrl from '../config.json'
 import { NavLink } from 'react-router-dom';
@@ -28,9 +28,8 @@ const RecentBlogs = () => {
     }, []);
     const len = data.length;
     const blog_1 = data[len - 1];
-    const blog_2 = [];
-    blog_2.push(data[len - 2]);
-    blog_2.push(data[len - 3]);
+    const blog_2 = data[len - 2];
+    const blog_3 = data[len - 3];
     return (
         <>
 
@@ -39,7 +38,7 @@ const RecentBlogs = () => {
             <div className="recent-blogs-container">
                     {isLoading && skeletons.map(skeleton => <center><CardSkeleton key={skeleton} /></center>)}
                     {blog_1 && blog_2 && <><div className="most-recent">
-                        <img src={ApiUrl + blog_1.cover} alt="/" className="l-img" />
+                        <img src={blogpost1} alt="/" className="l-img" />
                     <div className="m-blog-content">
                         <p className="timestamp">MohamedKaif. 20 Jan 2034</p>
                             <h3 className="blog-title" id="blog-title-L">{blog_1.title}</h3>
@@ -48,16 +47,27 @@ const RecentBlogs = () => {
                     </div>
                 </div>
                 <div className="recent-two">
-                            {blog_2.map(b => (<div className="recent-two-container" key={b._id}>
-                                <img src={ApiUrl + b.cover} alt="/" className="m-img" />
-                        <div className="blog-content">
-                                <h3 className="blog-title" id="blog-title-m">{b.title}</h3>
-                            <p className="timestamp">MohamedKaif. 20 Jan 2034</p>
-                                <p className="blog-description" id="blog-description-m">{b.summary}</p>
-                                <NavLink className="btn" to={`/blog/${b._id}`}>More...</NavLink>
+                            <div className="recent-two-container" key={blog_2._id}>
+                                <img src={blogpost2} alt="/" className="m-img" />
+                                <div className="blog-content" style={{ padding: '10px' }}>
+                                    <h3 className="blog-title" id="blog-title-m">{blog_2.title}</h3>
+                                    <p className="timestamp">MohamedKaif. 20 Jan 2034</p>
+                                    <p className="blog-description" id="blog-description-m">{blog_2.summary}</p>
+                                    <NavLink className="btn" to={`/blog/${blog_2._id}`}>More...</NavLink>
+                                </div>
+                            </div>
+                            <div className="recent-two-container" key={blog_3._id} >
+                                <img src={blogpost3} alt="/" className="m-img" />
+                                <div className="blog-content" style={{ padding: '10px' }}>
+                                    <h3 className="blog-title" id="blog-title-m">{blog_3.title}</h3>
+                                    <p className="timestamp">MohamedKaif. 20 Jan 2034</p>
+                                    <p className="blog-description" id="blog-description-m">{blog_3.summary}</p>
+                                    <NavLink className="btn" to={`/blog/${blog_3._id}`}>More...</NavLink>
+                                </div>
+                            </div>
+
                         </div>
-                        </div>))}
-                        </div></>}
+                    </>}
                 </div>
             </div>
 
